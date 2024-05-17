98051fa997614c0c2c094265c930576016864a99
sofiyo44 <162642897+sofiyo44@users.noreply.github.com>
2024-05-17 16:36:07 -0400

    Update checkoutStripe.js

--- js/checkoutStripe.js
+++ js/checkoutStripe.js
@@ -1,10 +1,8 @@
 // This is your test publishable API key.
 const stripe = Stripe("pk_test_51PHWHV09WcoQiYp2LY3F4iWLOiYKIpSNMNK0QPt58EIhgKcPC6xQucuZT7Dkeii1VTo0I0V9YKkoJUJHVah1MGG200JwErUvLp");
 
-initialize();
-
 // Create a Checkout Session
-async function initialize() {
+async function initializeCheckout() {
   const fetchClientSecret = async () => {
     const response = await fetch("/create-checkout-session", {
       method: "POST",
@@ -20,3 +18,10 @@ async function initialize() {
   // Mount Checkout
   checkout.mount('#checkout');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.proceed-to-checkout-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    initializeCheckout();
+  });
+});
