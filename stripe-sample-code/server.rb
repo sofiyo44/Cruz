4423ef37d1ae1d544431c9d65effce0c587b136c
sofiyo44 <162642897+sofiyo44@users.noreply.github.com>
2024-05-17 17:29:43 -0400

    Update server.rb

--- stripe-sample-code/server.rb
+++ stripe-sample-code/server.rb
@@ -1,5 +1,6 @@
 require 'stripe'
 require 'sinatra'
+require 'json'
 
 # This is your test secret API key.
 Stripe.api_key = 'sk_test_51PHWHV09WcoQiYp2tBwQxVQrlaWHTubfnsutAcM29zzOJMLDAZqg3qQ3RqRQqr3D3FkJqnFTDN1SQUXIXUBEantb00HbgxT6D0'
@@ -12,16 +13,25 @@ YOUR_DOMAIN = 'http://localhost:4242'
 post '/create-checkout-session' do
   content_type 'application/json'
 
+  # Parse the request body to get the totalReference
+  data = JSON.parse(request.body.read)
+  total_reference = data['totalReference']
+
   session = Stripe::Checkout::Session.create({
-    ui_mode: 'embedded',
+    payment_method_types: ['card'],
     line_items: [{
-      # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-      price: '{{PRICE_ID}}',
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'Total Amount',
+        },
+        unit_amount: total_reference, # Use the total amount from the request
+      },
       quantity: 1,
     }],
     mode: 'payment',
-    return_url: YOUR_DOMAIN + '/return.html?session_id={CHECKOUT_SESSION_ID}',
-    automatic_tax: {enabled: true},
+    success_url: YOUR_DOMAIN + '/success.html',
+    cancel_url: YOUR_DOMAIN + '/cancel.html',
   })
 
   {clientSecret: session.client_secret}.to_json
@@ -30,5 +40,5 @@ end
 get '/session-status' do
   session = Stripe::Checkout::Session.retrieve(params[:session_id])
 
-  {status: session.status, customer_email:  session.customer_details.email}.to_json
+  {status: session.status, customer_email: session.customer_details.email}.to_json
 end
